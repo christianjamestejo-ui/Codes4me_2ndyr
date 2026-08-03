a8632c03aea28d4e081ba399e88cc50999861b37
christianjamestejo-ui <christianjames.tejo@dbtc-cebu.edu.ph>
2026-08-03 17:29:58 +0800

    Hey Man New Hell

---
+++ OOPSamples/OOPSamples/Program.cs
@@ -0,0 +1,30 @@
+﻿using OOPSamplesStudent;
+using OOPSamplesTeacher;
+using System.Reflection.Emit;
+
+namespace OOPSamplesMain
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            //Constructor
+            Student _student = new Student(2, "Alexandra", "Soriano");
+
+           
+
+            Teacher _teacher = new Teacher();
+            _teacher.Id = 1;
+            _teacher.FirstName = "Mark";
+            _teacher.LastName = "Amioda";
+
+            _student.DisplayStudent();
+
+        }
+    }
+}
+
+
---
+++ OOPSamples/OOPSamples/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSamplesStudent
+{
+    public class Student
+    {
+        //private readonly DbContext _dbContext;
+
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        //Constructor
+        public Student(/*DbContext dbContext,*/ int _id, string? _firstname, string? _lastname)
+        {
+            //_dbContext = dbContext;
+            Id = _id;
+            FirstName = _firstname;
+            LastName = _lastname;
+        }
+
+        //Method
+        //public void UpdateStudentFirstName(string firstName) 
+        //{
+        //    FirstName = firstName;
+        //}
+
+        //public void UpdateStudentLastName(string lastName) 
+        //{
+        //    LastName = lastName;
+        //}
+
+        public void DisplayStudent() 
+        {
+            //String Interpolation
+            Console.WriteLine($"\nId: {Id}" +
+                $"\nFirst Name: {FirstName}" +
+                $"\nLast Name: {LastName}");
+        }
+
+    }
+}
